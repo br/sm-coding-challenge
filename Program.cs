2c678866415b544ba71a050565c5b4e1ae81a4fb
seetoe <kevinseetoe@gmail.com>
2023-11-02 20:49:20 -0400

    upgrade from dot net 3.1 to 7 and use minimal hosting model

--- Program.cs
+++ Program.cs
@@ -1,20 +1,28 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using sm_coding_challenge.Services.DataProvider;
 
-namespace sm_coding_challenge
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IDataProvider, DataProviderImpl>();
+
+var app = builder.Build();
+IWebHostEnvironment env = app.Environment;
+if (env.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStaticFiles();
+app.UseAuthorization();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.Run();
